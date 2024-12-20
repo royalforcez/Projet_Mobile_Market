702c87dbedd6e3f966d6849ad48ddd8e4901a2b6
CazinSacha <cazin.lep@gmail.com>
2024-12-20 10:23:16 +0100

    compilesdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.projet_mobile_market"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projet_mobile_market"
