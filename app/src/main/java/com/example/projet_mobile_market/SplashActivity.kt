dc2aa51135147701e0feb8a874ae9af3b16c79e8
CazinSacha <cazin.lep@gmail.com>
2025-01-02 10:59:08 +0100

    SplashActivity background and alignment

--- app/src/main/java/com/example/projet_mobile_market/SplashActivity.kt
+++ app/src/main/java/com/example/projet_mobile_market/SplashActivity.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.projet_mobile_market.ui.theme.Projet_Mobile_MarketTheme
 
@@ -32,10 +37,13 @@ class SplashActivity : ComponentActivity() {
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (
+        modifier = modifier.background(Color.White).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ){
+
+    }
 }
 
 @Preview(showBackground = true)
