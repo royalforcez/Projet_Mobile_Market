bb8e88ea05b9f3ea1219f40f6810935246230991
CazinSacha <cazin.lep@gmail.com>
2025-01-02 11:16:42 +0100

    Add Logo SplashActiviy

--- app/src/main/java/com/example/projet_mobile_market/SplashActivity.kt
+++ app/src/main/java/com/example/projet_mobile_market/SplashActivity.kt
@@ -1,13 +1,19 @@
 package com.example.projet_mobile_market
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,7 +21,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.projet_mobile_market.ui.theme.Projet_Mobile_MarketTheme
 
 class SplashActivity : ComponentActivity() {
@@ -32,6 +41,10 @@ class SplashActivity : ComponentActivity() {
                 }
             }
         }
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        },1500)
     }
 }
 
@@ -42,7 +55,14 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ){
-
+        Image(
+            painter = painterResource(id = R.drawable.p_s_c_marketplace_1),
+            contentDescription = "Logo Market",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
     }
 }
 
