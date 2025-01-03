c5ca103f811d98bb2d7059fa240b9a5b15ab4dd4
CazinSacha <cazin.lep@gmail.com>
2025-01-03 16:10:55 +0100

    Ajout de l'API à FrozenActivity.kt

--- app/src/main/java/com/example/projet_mobile_market/FrozenActivity.kt
+++ app/src/main/java/com/example/projet_mobile_market/FrozenActivity.kt
@@ -1,34 +1,30 @@
 package com.example.projet_mobile_market
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
@@ -44,51 +40,115 @@ class FrozenActivity : ComponentActivity() {
 }
 
 @Composable
-fun FrozenScreen(
-    name: String = "Surgelés",
-    modifier: androidx.compose.ui.Modifier = androidx.compose.ui.Modifier
-) {
+fun FrozenScreen() {
+    val frozens = remember { mutableStateListOf<Frozen>() }
 
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        content = { innerPadding ->
-            Column(
-                modifier = androidx.compose.ui.Modifier
-                    .fillMaxSize()
-                    .padding(innerPadding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Top
-            ) {
-                // Header avec un dégradé
-                Column(
-                    modifier = androidx.compose.ui.Modifier
-                        .fillMaxWidth()
-                        .background(
-                            Brush.horizontalGradient(
-                                colors = listOf(Color(0xFFFF5722), Color(0xFFFFC107)) // Dégradé orange/jaune
-                            )
-                        )
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = name,
-                        color = Color.White,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
+    LaunchedEffect(Unit) {
+        val data = fetchFrozenData()
+        frozens.addAll(data)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        // Header avec un dégradé
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Brush.horizontalGradient(
+                        colors = listOf(Color(0xFFFF5722), Color(0xFFFFC107))
                     )
-                }
+                )
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Surgelés",
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        }
 
+        // Liste des boissons
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            frozens.forEach { frozen ->
+                FrozenCard(frozen)
             }
         }
-    )
+    }
 }
 
-
-
-@Preview(showBackground = true)
 @Composable
-fun FrozenScreenPreview() {
-    FrozenScreen()
+fun FrozenCard(frozen: Frozen) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+
+        ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = rememberImagePainter(frozen.imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(64.dp)
+                    .padding(end = 16.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Column {
+                Text(
+                    text = frozen.name,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                Text(
+                    text = frozen.description,
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 14.sp
+                    ),
+                    modifier = Modifier.padding(top = 4.dp),
+                    maxLines = 2
+                )
+            }
+        }
+    }
 }
+
+suspend fun fetchFrozenData(): List<Frozen> = withContext(Dispatchers.IO) {
+    val url = "https://api.jsonbin.io/v3/b/67603408e41b4d34e466500a"
+    val response = URL(url).readText()
+    val jsonObject = JSONObject(response)
+    val frozenArray = jsonObject.getJSONArray("record")
+
+    List(frozenArray.length()) { i ->
+        val frozenObject = frozenArray.getJSONObject(i)
+        Frozen(
+            name = frozenObject.getString("name"),
+            description = frozenObject.getString("description"),
+            imageUrl = frozenObject.getString("picture_url")
+        )
+    }
+}
+
+data class Frozen(
+    val name: String,
+    val description: String,
+    val imageUrl: String
+)
